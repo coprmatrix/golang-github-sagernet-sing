8c7cab6db8f47d11007a280af1796fcbd08b187f
u <u>
2025-04-05 09:30:33 +0300

    update

--- regex.pl
+++ regex.pl
@@ -1,4 +1,4 @@
-s~%global\s+goipath\s+(.*)/(.*)~%global goihead \2\n%global goipath \1/%{goihead}~g;
+s~%global\s+goipath\s+(.*)/(.*)~%global goihead \2\n%global goipath \1/%{goihead}\n%define scom %{?commit}%{?!commit:v%{version}}~g;
 s~(Version:)[^0-9\.]*(.*)~\1  \2~g;
 s~%gocheck~~g;
-s~Source:.*~%define stag %{?tag}%{?!tag:%commit}\nSource: https://%{goipath}/archive/%{stag}/%{goihead}-%{stag}.tar.gz~g;
+s~Source:.*~%define stag %{?tag}%{?!tag:%scom}\nSource: https://%{goipath}/archive/%{stag}/%{goihead}-%{stag}.tar.gz~g;
